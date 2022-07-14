2b74595c31bd86c426259626966c65ce70c79eaf
coolviadislav <coolviadislav98@gmail.com>
2022-07-14 13:16:44 +0300

    подключил croler

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php
-require_once "vendor/autoload.php";
+require('vendor/autoload.php');
 
 use Symfony\Component\DomCrawler\Crawler;
 
-$html = <<<'HTML'
-<!DOCTYPE html>
-<html>
-    <body>
-        <p class="message">Hello World!</p>
-        <p>Hello Crawler!</p>
-    </body>
-</html>
-HTML;
+$html = "https://www.strd.ru/kirpich/";
 
 $crawler = new Crawler($html);
 
-foreach ($crawler as $domElement) {
-    var_dump($domElement->nodeName);
-}
+$name = $crawler->filter('p.crumbs')->first()->text();
+
+var_dump($name);
